806cb468135c9b9d90df1115875b8be5498b5798
Fernando L <aleguia88@hotmail.com>
2022-05-11 10:50:57 -0400

    Handling new lines when passed to add function

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -3,17 +3,28 @@
 */
 object Calculator {
     fun add(numbers: String): Int {
-        if (numbers.isEmpty()) return 0
+        val sanitizedNumbers = handleNewLines(numbers)
+        if (sanitizedNumbers.isEmpty()) return 0
 
 
-        val numbersList = numbers.split(',')
+        val numbersList = sanitizedNumbers.split(',')
             .map { stringNumber -> Integer.parseInt(stringNumber) }
 
         return numbersList.sum()
     }
+
+    private fun handleNewLines(text: String): String{
+        return text.replace("\n", "")
+    }
 }
 
 fun main(args: Array<String>){
-    val input = "1,2,5"
+    var input = "1,2,5"
+    println("Input: $input Result: ${Calculator.add(input)}")
+
+    input = "1\n,2,3"
+    println("Input: $input Result: ${Calculator.add(input)}")
+
+    input = "1,\n2,4"
     println("Input: $input Result: ${Calculator.add(input)}")
 }
